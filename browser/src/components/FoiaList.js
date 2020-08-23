71df2af9f33f6f674ca539b4556f336286f5aac9
Ash Ryan Arnwine <accounts@ashryan.io>
2020-08-23 15:19:09 -0400

    Basic component.

---
+++ browser/src/components/FoiaList.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+const FoiaList = () => {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:3000/v1/")
+      .then((data) => data.json())
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+
+  return data ? (
+    <p>Data length: {data && data.foiaList.length}</p>
+  ) : (
+    <p>No data.</p>
+  );
+};
+
+export default FoiaList;
