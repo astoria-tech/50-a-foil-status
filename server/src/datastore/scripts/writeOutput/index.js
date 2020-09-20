d71c61b5d832a333c8d1cedf0d7178f024692c38
Ash Ryan Arnwine <accounts@ashryan.io>
2020-09-20 14:49:54 -0400

    Ensuring all Muckrock ids are stored as strings instead of numbers.

--- server/src/datastore/scripts/writeOutput/index.js
+++ server/src/datastore/scripts/writeOutput/index.js
@@ -13,11 +13,15 @@ const writeOutput = async (dataStore) => {
   }
 
   try {
-    await fs.writeFile(`${dirname}/${filename}`, JSON.stringify(dataStore));
+    await fs.writeFile(`${dirname}/${filename}`, JSON.stringify(dataStore, convertIntIdsToString));
     console.log(`Output written to ${dirname}/${filename}!`);
   } catch (error) {
     console.log("Unable to write error log file", error);
   }
+};
+
+const convertIntIdsToString = (key, value) => {
+  return key === "id" && typeof value === "number" ? value.toString() : value;
 };
 
 module.exports = writeOutput;
