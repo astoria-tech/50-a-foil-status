73a69f8045f397fbf91f9d1f32d97cca33fdfa37
Chris DeLuca <code@chrisdeluca.me>
2020-11-07 19:36:42 -0500

    Add cost bar graph
    This includes the basic functionality for a static bar graph displaying
    costs.
    
    We are displaying very simple data, only one axis, but the nivo graph
    library does not offer a way to hide either x or y axis, only bar
    labels. While it is probably better visually to display *only* bar
    labels, nivo does not support this at this time.
    
    As a workaround, hide the labels, relying on the y axis for value
    display, and hide the x axis via a brittle CSS selector; nivo also does
    not add or allow adding identifiers to elements, so the selector used is
    brittle because it is purely positional. If the source mark shipped from
    the nivo library changes, this selector could break.

--- browser/src/App.js
+++ browser/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useAsync } from "react-async";
 import FoiaList from "./components/FoiaList";
+import { Bar } from "@nivo/bar";
+import "./styles.css";
 
 async function getMuckrockData() {
   return await fetch("http://localhost:3000/v1/latest")
@@ -14,8 +16,36 @@ function App() {
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
   if (!data) return <p>No data</p>;
 
+  let uniquePrices = new Set();
+  data.foiaList.forEach(item => {
+    const price = parseFloat(item.foiaReq.price);
+    if (price <= 0) return false;
+    uniquePrices.add(price);
+  });
+  const prices = Array.from(uniquePrices)
+    .sort((first, second) => second > first)
+    .map(item => {
+      return {
+        value: item,
+        id: new Intl.NumberFormat(navigator.language, {
+          style: 'currency',
+          currency: 'USD'
+        }).format(item)
+      }
+    });
+
   return (
     <div className="App">
+      <h2 className="headline__costs">Range of 50a request costs</h2>
+      <Bar
+        width={800}
+        height={500}
+        isInteractive={false}
+        margin={{ top: 0, right: 100, bottom: 50, left: 100 }}
+        layout="horizontal"
+        enableLabel={false}
+        data={prices}
+      />
       <FoiaList data={data} />
     </div>
   );
