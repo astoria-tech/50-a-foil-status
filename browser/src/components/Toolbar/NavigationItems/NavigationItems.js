4cb525537bbd53aa591e1d2df1d77867f838847d
Chris DeLuca <code@chrisdeluca.me>
2021-02-27 15:10:40 -0500

    Fix nav items markup indentation

--- browser/src/components/Toolbar/NavigationItems/NavigationItems.js
+++ browser/src/components/Toolbar/NavigationItems/NavigationItems.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const NavigationItems = () => {
-
-	return (
-		<div  className="navigation_items">
-			<ul>
+  return (
+    <div  className="navigation_items">
+      <ul>
         <li><a href="#about_these_requests">About These Requests</a></li>
         <li><a href="http://www.muckrock.com/">Muckrock</a></li>
         <li><a href="https://www.muckrock.com/foi/list/?projects=778">All Requests</a></li>
@@ -12,10 +11,9 @@ const NavigationItems = () => {
         <li><a href="https://astoria.digital/">Astoria Digital</a></li>
         <li><a href="https://github.com/astoria-tech/50-a-foil-status">Github for this project</a></li>
         <li><a href="https://foil.astoria.digital/">FOIL Requests in New York State</a></li>
-			</ul>
-		</div>
-	);
+      </ul>
+    </div>
+  );
 };
-
 
 export default NavigationItems
